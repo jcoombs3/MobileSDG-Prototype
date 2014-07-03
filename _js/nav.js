81a155f3196883afcc0ee16b3c6adf1eabc8c7c4
khosford <khosford@sapient.com>
2014-07-03 11:17:10 -0400

    bust state things

--- _js/nav.js
+++ _js/nav.js
@@ -17,8 +17,7 @@ $(window).load(function(){
                 TweenMax.to($('.nav-overlay'), 1, {opacity:'0.5', ease:Back.easeOut});
 
                 TweenMax.to($('.list-section li'), 0.1, {borderColor:'#DfE5E6'});
-                TweenMax.to($('.list-section li a'), 0.5, {opacity:'1'});
-                window.setTimeout(function(){$('#nav').removeClass('busy')}, 500);
+                TweenMax.to($('.list-section li a'), 0.5, {opacity:'1', onComplete:function(){$('#nav').removeClass('busy');}});
 
             }
         }
@@ -35,11 +34,10 @@ $(window).load(function(){
                     TweenMax.to($('.nav-overlay'), 1, {opacity:'0', ease:Back.easeIn, onComplete:function(){
                         TweenMax.to($('#nav'), 0, {width:navWidthMedia+'px'});
                     }});
-                    TweenMax.to($('.nav-container'), 0.5, {width:navWidthMedia+'px', ease:Back.easeIn});
+                    TweenMax.to($('.nav-container'), 0.5, {width:navWidthMedia+'px', ease:Back.easeIn, onComplete:function(){$('#nav').removeClass('busy');}});
                     
                     TweenMax.to($('.list-section li a'), 0.5, {opacity:'0'});
-                    TweenMax.to($('.list-section li'), 0.1, {borderColor:'#D32728'}); 
-                window.setTimeout(function(){$('#nav').removeClass('busy')}, 500);
+                    TweenMax.to($('.list-section li'), 0.1, {borderColor:'#D32728'});
                 }     
             }
             else {
