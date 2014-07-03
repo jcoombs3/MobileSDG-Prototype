dc6534e438120ce5d084fd68760b04f9c3ea83cd
jcoombs3 <jcoombs@sapient.com>
2014-07-03 11:05:49 -0400

    grabbing

--- _js/nav.js
+++ _js/nav.js
@@ -12,17 +12,21 @@ $(window).load(function(){
             TweenMax.to($('.list-section'), 0, {opacity:'1'});
 
             TweenMax.to($('.nav-container'), 0.5, {width:navWidth+'px', ease:Back.easeOut});
-            TweenMax.to($('.nav-overlay'), 1, {opacity:'0.5', ease:Back.easeOut});
+            TweenMax.to($('.nav-overlay'), 1, {opacity:'1', ease:Back.easeOut});
 
             TweenMax.to($('.list-section li'), 0.1, {borderColor:'#DfE5E6'});
             TweenMax.to($('.list-section li a'), 0.5, {opacity:'1'});
+
+
+            TweenMax.staggerFrom($('.tile'),0.2,{rotationY:'90deg'},0.005);
         }
         else {
 
             if($(e.target).hasClass('nav-overlay') 
                 || $(e.target).hasClass('nav-link') 
                 || $(e.target).hasClass('link') 
-                || $(e.target).hasClass('icon')){
+                || $(e.target).hasClass('icon')
+                || $(e.target).hasClass('tile')){
                 
                 TweenMax.to($('.nav-overlay'), 1, {opacity:'0', ease:Back.easeIn, onComplete:function(){
                     TweenMax.to($('#nav'), 0, {width:navWidthMedia+'px'});
