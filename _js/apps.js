9ce36569d5a01784787793d475652b8279ea8d48
jcoombs3 <jcoombs@sapient.com>
2014-07-07 16:14:42 -0400

    added javadoc to apps.js

--- _js/apps.js
+++ _js/apps.js
@@ -4,22 +4,31 @@ $(window).load(function(){
         stackDeck(e);
     });
 
-
     $('#apps .back').on('click',function(){
         unstackDeck();
     });
     
 });
 
-function stackDeck(e) {
-    var order = $(e.currentTarget).data('order');
+/**
+* This method stacks the apps under the selected app, creating a card stack animation
+*
+* @param    li the selected list item (app)
+*
+* @var order within the ul, what is the selected app's placement 
+* @var delay hard coded delay variable that stacks depending on how far each other app is away from the selected app 
+* @var offset selected app's position in device (left and top)
+* @var posY selected app's y position
+* @var maxDelay finding the farthest delay possible for the centerApps function, since we cannot use an OnComplete function on the staggered Tweens
+*/
+function stackDeck(li) {
+    var order = $(li.currentTarget).data('order');
     var delay = 0.03;
-    var offset = $(e.currentTarget).offset();
+    var offset = $(li.currentTarget).offset();
     var posY = offset.top;
-
     var maxDelay = 0;
 
-    $(e.currentTarget).addClass('active');
+    $(li.currentTarget).addClass('active');
 
     $('#apps li').each(function(){
         var orderDelta = Math.abs($(this).data('order') - order);
@@ -38,10 +47,16 @@ function stackDeck(e) {
     $('#apps .back').addClass('reveal');
     $('#apps ul').addClass('sticky');
 
-    centerApp($(e.currentTarget), maxDelay);
+    centerApp($(li.currentTarget), maxDelay);
 }
 
-
+/**
+* This method unstacks the apps and resets the ul. 
+*
+* @var target selected list item (app)
+* @var order within the ul, what is the selected app's placement 
+* @var delay hard coded delay variable that stacks depending on how far each other app is away from the selected app 
+*/
 function unstackDeck() {
     var target = $('#apps ul').find('.active');
     var order = $(target).data('order');
@@ -65,15 +80,37 @@ function unstackDeck() {
     $('#apps ul').removeClass('sticky');
 }
 
+/**
+* This method centers the ul and list item centered in its scrollable area for alignment purposes
+*
+* @var liHeight half of the selected app's height for centering purposes 
+* @var windowHeight half of the device's height for centering purposes
+* @var offset selected app's position in device (left and top)
+* @var posY selected app's y position
+* @var offKilter if the app selected is off the viewable area in any form, we use this variable to make sure when the ul is given overflow:hidden, the whole app is shown instead of cut off.
+*/
 function centerApp(li, maxDelay) {
     var liHeight = $(li).outerHeight()/2;
     var windowHeight = $(window).outerHeight()/2;
     var offset = $(li).offset();
     var posY = offset.top;
+    var offKilter = 0;
+
+    if(posY < 0) {
+        TweenMax.to($(li),0.1,{top:-1*posY});
+        offKilter = posY;
+    }
+    else if(posY+liHeight*2 > $(window).outerHeight()){
+        var delta = $(window).outerHeight() - posY;
+        var posTop = liHeight*2 - delta; 
+        TweenMax.to($(li),0.1,{top:-posTop});
+        offKilter = posTop;
+    }
+
     var newTop = windowHeight - posY - liHeight;
     maxDelay+=0.5;
 
-    TweenMax.to($('#apps ul'),0.25,{top:newTop+'px', delay:maxDelay, onComplete:function(){
+    TweenMax.to($('#apps ul'),0.25,{top:newTop+offKilter+'px', delay:maxDelay, onComplete:function(){
         $(li).addClass('load-state');
     }});
 }
