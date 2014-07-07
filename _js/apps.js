2245ff1189e0419cee1ffe17facf71ea79f4eb6f
jcoombs3 <jcoombs@sapient.com>
2014-07-07 13:57:10 -0400

    unstack

--- _js/apps.js
+++ _js/apps.js
@@ -3,6 +3,11 @@ $(window).load(function(){
     $('#apps li').on('click',function(e){
         stackDeck(e);
     });
+
+
+    $('#apps .back').on('click',function(){
+        unstackDeck();
+    });
     
 });
 
@@ -24,6 +29,27 @@ function stackDeck(e) {
         TweenMax.to($(this),0.2,{top:'-='+posYDelta+'px', opacity:'0', delay:delayDelta, ease:Back.easeIn});
     
     });
+
+    $('#apps .back').addClass('reveal');
+}
+
+
+function unstackDeck() {
+    var target = $('#apps ul').find('.active');
+    var order = $(target).data('order');
+    var delay = 0.03;
+    
+    $('#apps li').each(function(){
+        var orderDelta = Math.abs($(this).data('order') - order);
+        var delayDelta = delay * orderDelta;
+        
+        TweenMax.to($(this),0.2,{top:'0', opacity:'1', delay:delayDelta, ease:Back.easeIn, onComplete:function(){
+            $(target).removeClass('active');
+        }});
+    
+    });
+
+    $('#apps .back').removeClass('reveal');
 }
 
 
