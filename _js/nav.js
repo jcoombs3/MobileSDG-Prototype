962cc406fc95c3ccb77a9c4bffa1b748df4f592d
khosford <khosford@sapient.com>
2014-07-07 10:38:03 -0400

    nag animo 'busy' state n stoof

--- _js/nav.js
+++ _js/nav.js
@@ -3,9 +3,15 @@ $(window).load(function(){
     var navWidth = 192;
     var navWidthMedia = 50;
 
-    $('#nav').on('click',function(e){
-        $('#nav').toggleClass('expanded');
+    var openNav = false;
+    if($('#nav').outerWidth() >= navWidth){
+        openNav = true;
+    }
 
+    $('#nav').on('click',function(e){
+        if(!openNav){
+            $('#nav').toggleClass('expanded');
+        }
         if($('#nav').hasClass('expanded')){
             if(!$('#nav').hasClass('busy')){
                 $('#nav').addClass('busy');
@@ -17,15 +23,11 @@ $(window).load(function(){
                 TweenMax.to($('.nav-overlay'), 1, {opacity:'1', ease:Back.easeOut});
 
                 TweenMax.to($('.list-section li'), 0.1, {borderColor:'#DfE5E6'});
-<<<<<<< HEAD
-                TweenMax.to($('.list-section li a'), 0.5, {opacity:'1', onComplete:function(){$('#nav').removeClass('busy');}});
-=======
                 TweenMax.to($('.list-section li a'), 0.5, {opacity:'1'});
 
                 TweenMax.staggerFrom($('.tile'),0.2,{rotationY:'90deg'},0.005);
 
-                window.setTimeout(function(){$('#nav').removeClass('busy')}, 500);
->>>>>>> FETCH_HEAD
+                window.setTimeout(function(){$('#nav').removeClass('busy')}, 1000);
 
             }
         }
@@ -35,14 +37,9 @@ $(window).load(function(){
                 || $(e.target).hasClass('nav-link') 
                 || $(e.target).hasClass('link') 
                 || $(e.target).hasClass('icon')
-                || $(e.target).hasClass('tile')){
-                
-                TweenMax.to($('.nav-overlay'), 1, {opacity:'0', ease:Back.easeIn, onComplete:function(){
-                    TweenMax.to($('#nav'), 0, {width:navWidthMedia+'px'});
-                }});
-                TweenMax.to($('.nav-container'), 0.5, {width:navWidthMedia+'px', ease:Back.easeIn});
-                
-                TweenMax.to($('.list-section li a'), 0.5, {opacity:'0'});
+                || $(e.target).hasClass('tile')
+                || $(e.target).hasClass('nav-container')
+                || $(e.target).hasClass('logo-block')){
 
                 if(!$('#nav').hasClass('busy')){
                     $('#nav').addClass('busy');
@@ -53,16 +50,13 @@ $(window).load(function(){
                     TweenMax.to($('.nav-container'), 0.5, {width:navWidthMedia+'px', ease:Back.easeIn, onComplete:function(){$('#nav').removeClass('busy');}});
                     
                     TweenMax.to($('.list-section li a'), 0.5, {opacity:'0'});
-<<<<<<< HEAD
                     TweenMax.to($('.list-section li'), 0.1, {borderColor:'#D32728'});
-=======
-                    TweenMax.to($('.list-section li'), 0.1, {borderColor:'#D32728'}); 
-                    window.setTimeout(function(){$('#nav').removeClass('busy')}, 500);
->>>>>>> FETCH_HEAD
                 }     
             }
             else {
-                $('#nav').addClass('expanded');
+                if(!openNav){
+                    $('#nav').addClass('expanded');
+                }
             }
         }
     });
